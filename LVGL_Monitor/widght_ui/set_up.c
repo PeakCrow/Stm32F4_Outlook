8123513d71b46cbeff69223f6c5974d07d2dc0ed
family_xian <tian_runxian@163.com>
2022-10-13 23:03:20 +0800

    增加按钮动画

--- LVGL_Monitor/widght_ui/set_up.c
+++ LVGL_Monitor/widght_ui/set_up.c
@@ -16,31 +16,31 @@ void Set_Up_Ui(lv_obj_t *parent)
     lv_obj_t* Imgbtn_MC;
     Imgbtn_MC = lv_imgbtn_create(parent);
     /* 设置按钮释放时的图像 */
-    lv_imgbtn_set_src(Imgbtn_MC,LV_STATE_DEFAULT,NULL,"E:/Ls_Monitor/LVGL_Monitor/images/set_up.png",NULL);
+    lv_imgbtn_set_src(Imgbtn_MC,LV_STATE_DEFAULT,NULL,"E:/Ls_Monitor/LVGL_Monitor/images/set_up.png","E:/Ls_Monitor/LVGL_Monitor/images/set_up.png");
     /* 设置按钮大小 */
     lv_obj_set_size(Imgbtn_MC,400,120);
     /* 设置按钮位置 */
     lv_obj_align_to(Imgbtn_MC,parent,LV_ALIGN_BOTTOM_LEFT,0,0);
-    /* 普通样式将透明度设置为50 */
-    static lv_style_prop_t tr_prop[] = {LV_STYLE_TRANSLATE_Y,
-                                        LV_STYLE_IMG_RECOLOR_OPA,
-                                        LV_STYLE_PROP_INV};
+    /*Create a transition animation on width transformation and recolor.*/
+    static lv_style_prop_t tr_prop[] = {LV_STYLE_TRANSFORM_WIDTH, LV_STYLE_IMG_RECOLOR_OPA, 0};
     static lv_style_transition_dsc_t tr;
-    lv_style_transition_dsc_init(&tr, tr_prop, lv_anim_path_linear,
-    100, 0, NULL);
-    /* 添加按钮释放风格 */
-    lv_img_set_antialias(Imgbtn_MC,true);
-    lv_obj_set_style_transition(Imgbtn_MC, &tr, LV_STATE_DEFAULT);
-    lv_style_init(&s_style_common);
-    lv_style_set_opa(&s_style_common,LV_OPA_50);
-    //lv_obj_set_ext_click_area(Imgbtn_MC,-100);
-    /* 普通样式将图片按钮缩小%20 */
-    lv_obj_set_style_img_recolor(Imgbtn_MC,lv_palette_main(LV_PALETTE_BLUE),LV_INDEV_STATE_RELEASED);
-    //lv_obj_set_style_translate_y(Imgbtn_MC,50,LV_INDEV_STATE_PRESSED);
-    lv_style_set_transform_angle(&s_style_common,450);
-    //lv_style_set_transform_zoom(&s_style_common,128);
-    /* 设置按钮按下时的样式 */
+    lv_style_transition_dsc_init(&tr, tr_prop, lv_anim_path_linear, 200, 0, NULL);
+
+    static lv_style_t style_def;
+    lv_style_init(&style_def);
+    lv_style_set_text_color(&style_def, lv_color_white());
+    lv_style_set_transition(&style_def, &tr);
     lv_obj_add_style(Imgbtn_MC,&s_style_common,LV_STATE_PRESSED);
+
+    /*Darken the button when pressed and make it wider*/
+    static lv_style_t style_pr;
+    lv_style_init(&style_pr);
+    lv_style_set_img_recolor_opa(&style_pr, LV_OPA_30);
+    lv_style_set_img_recolor(&style_pr, lv_color_black());
+    lv_style_set_transform_width(&style_pr, 20);
+
+    lv_obj_add_style(Imgbtn_MC,&style_def,0);
+    lv_obj_add_style(Imgbtn_MC,&style_pr,LV_STATE_PRESSED);
     /* 设置按钮回调 */
     lv_obj_add_event_cb(Imgbtn_MC,Imgbtn_MC_cb,LV_EVENT_ALL,NULL);
 }
