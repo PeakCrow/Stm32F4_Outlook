65ae2e59785dc6b04d247e856ef3da98e4dd5a14
family_xian <tian_runxian@163.com>
2022-11-01 22:17:29 +0800

    添加42电机正反转屏蔽按钮

--- LVGL_Monitor/widght_ui/adjust_pedal.c
+++ LVGL_Monitor/widght_ui/adjust_pedal.c
@@ -9,6 +9,9 @@ static lv_style_t s_style_common;
 static void Adjust_Pedal_In_Ui(lv_obj_t* parent);
 static void Forward_Btn_Cb(lv_event_t* e);
 static void Reverse_Btn_Cb(lv_event_t* e);
+static void sw_event_cb(lv_event_t * e);
+static lv_obj_t * forward_btn;
+static lv_obj_t * reverse_btn;
 
 
 void Adjust_Pedal_Ui(lv_obj_t *parent)
@@ -55,30 +58,88 @@ static void Imgbtn_MC_cb(lv_event_t * e)
 
 static void Adjust_Pedal_In_Ui(lv_obj_t* parent)
 {
-    lv_obj_t * forward_btn;
-    lv_obj_t * reverse_btn;
     lv_obj_t * for_label;
     lv_obj_t * rev_label;
+    lv_obj_t * sw_label;
+    /* 样式配置 */
+    /*Properties to transition*/
+    static lv_style_prop_t props[] = {
+        LV_STYLE_TRANSFORM_WIDTH, LV_STYLE_TRANSFORM_HEIGHT, LV_STYLE_TEXT_LETTER_SPACE, 0
+    };
 
+    /*Transition descriptor when going back to the default state.
+     *Add some delay to be sure the press transition is visible even if the press was very short*/
+    static lv_style_transition_dsc_t transition_dsc_def;
+    lv_style_transition_dsc_init(&transition_dsc_def, props, lv_anim_path_overshoot, 250, 100, NULL);
+
+    /*Transition descriptor when going to pressed state.
+     *No delay, go to presses state immediately*/
+    static lv_style_transition_dsc_t transition_dsc_pr;
+    lv_style_transition_dsc_init(&transition_dsc_pr, props, lv_anim_path_ease_in_out, 250, 0, NULL);
+
+    /*Add only the new transition to he default state*/
+    static lv_style_t style_def;
+    lv_style_init(&style_def);
+    lv_style_set_transition(&style_def, &transition_dsc_def);
+
+    /*Add the transition and some transformation to the presses state.*/
+    static lv_style_t style_pr;
+    lv_style_init(&style_pr);
+    lv_style_set_transform_width(&style_pr, 20);
+    lv_style_set_transform_height(&style_pr, -20);
+    lv_style_set_text_letter_space(&style_pr, 20);
+    lv_style_set_transition(&style_pr, &transition_dsc_pr);
+
+    /* 正转按钮 */
     forward_btn = lv_btn_create(parent);
     lv_obj_set_size(forward_btn,160,100);
-    lv_obj_align_to(forward_btn,parent,LV_ALIGN_CENTER,-150,0);
+    lv_obj_align_to(forward_btn,parent,LV_ALIGN_CENTER,-150,-100);
     for_label = lv_label_create(parent);
     lv_label_set_text(for_label,"FORWARD");
+    lv_obj_set_style_bg_color(forward_btn,lv_palette_main(LV_PALETTE_YELLOW),LV_STATE_DEFAULT);
     lv_obj_set_style_text_font(for_label,&lv_font_montserrat_14,LV_PART_MAIN);
     lv_obj_align_to(for_label,forward_btn,LV_ALIGN_CENTER,0,0);
     lv_obj_add_event_cb(forward_btn,Forward_Btn_Cb,LV_EVENT_ALL,NULL);
+    lv_obj_add_style(forward_btn,&style_pr,LV_STATE_PRESSED);
+    lv_obj_add_style(forward_btn,&style_def,0);
 
+    /* 反转按钮 */
     reverse_btn = lv_btn_create(parent);
     lv_obj_set_size(reverse_btn,160,100);
-    lv_obj_align_to(reverse_btn,parent,LV_ALIGN_CENTER,150,0);
+    lv_obj_align_to(reverse_btn,parent,LV_ALIGN_CENTER,150,-100);
     rev_label = lv_label_create(parent);
     lv_label_set_text(rev_label,"REVERSE");
+    lv_obj_set_style_bg_color(reverse_btn,lv_palette_main(LV_PALETTE_GREEN),LV_STATE_DEFAULT);
     lv_obj_set_style_text_font(rev_label,&lv_font_montserrat_14,LV_PART_MAIN);
     lv_obj_align_to(rev_label,reverse_btn,LV_ALIGN_CENTER,0,0);
     lv_obj_add_event_cb(reverse_btn,Reverse_Btn_Cb,LV_EVENT_ALL,NULL);
-}
+    lv_obj_add_style(reverse_btn,&style_pr,LV_STATE_PRESSED);
+    lv_obj_add_style(reverse_btn,&style_def,0);
 
+    lv_obj_t * sw = lv_switch_create(parent);
+    lv_obj_set_pos(sw,680,40);
+    lv_obj_add_state(sw, LV_STATE_CHECKED);//LV_STATE_CHECKED
+    sw_label = lv_label_create(parent);
+    lv_obj_align_to(sw_label,sw,LV_ALIGN_OUT_BOTTOM_MID,-25,0);
+    lv_label_set_text(sw_label,"Adjust On");
+    lv_obj_add_event_cb(sw, sw_event_cb, LV_EVENT_VALUE_CHANGED, sw_label);
+}
+static void sw_event_cb(lv_event_t * e)
+{
+    lv_obj_t * sw = lv_event_get_target(e);
+    lv_obj_t * sw_label = lv_event_get_user_data(e);
+    if(lv_obj_has_state(sw, LV_STATE_CHECKED)) {/* 关闭状态 */
+        lv_label_set_text(sw_label,"Adjust On");
+        lv_obj_add_flag(forward_btn,LV_OBJ_FLAG_CLICKABLE);
+        lv_obj_add_flag(reverse_btn,LV_OBJ_FLAG_CLICKABLE);
+    }
+    else {/* 默认打开 */
+        lv_label_set_text(sw_label,"Adjust Off");
+        lv_obj_clear_flag(reverse_btn,LV_OBJ_FLAG_CLICKABLE);
+        lv_obj_clear_flag(forward_btn,LV_OBJ_FLAG_CLICKABLE);
+    }
+}
+/* 正转按钮回调函数 */
 static void Forward_Btn_Cb(lv_event_t* e)
 {
     lv_event_code_t code = lv_event_get_code(e);
@@ -89,6 +150,7 @@ static void Forward_Btn_Cb(lv_event_t* e)
         printf("1--houtui\n");
 
 }
+/* 反转按钮回调函数 */
 static void Reverse_Btn_Cb(lv_event_t* e)
 {
     lv_event_code_t code = lv_event_get_code(e);
